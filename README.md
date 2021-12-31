1c09774cb6ab9d82e09d8e020548ac397176da2b
NotDSF <67711164+NotDSF@users.noreply.github.com>
2021-12-31 02:55:46 +0000

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+# Luamin
+[Luamin.js](https://github.com/Herrtt/luamin.js) but in a vscode extension.
+
+## Features
+- Beautify
+- Minify
+- Uglify
+
+## Options
+- Rename Variables
+> Rename variable names (eg. `iIiiIi` into `L_1`)
+
+- Rename Globals
+> Rename global names (eg. `iIiiIi` into `G_1`) (not recommened)
+
+- Solve Math
+> Automatically solves math (eg. `1 + 3 + 4` into `8`)
---
+++ extension.js
@@ -0,0 +1,65 @@
+const vscode = require("vscode");
+const luamin = require("./luamin.js");
+
+let Options = [
+	{
+		label: "Rename Variables",
+		description: "Rename variable names (eg. `iIiiIi` into `L_1`)",
+		picked: true
+	},
+	{
+		label: "Rename Globals",
+		description: "Rename global names (eg. `iIiiIi` into `G_1`) (not recommened)",
+		picked: false
+	},
+	{
+		label: "Solve Math",
+		description: "Automatically solves math (eg. `1 + 3 + 4` into `8`)",
+		picked: true
+	}
+];
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+	let command = vscode.commands.registerCommand('luamin.luamin', async function () {
+		let Script = vscode.window.activeTextEditor.document.getText();
+		if (!Script.length) return vscode.window.showErrorMessage("No script content in current file");
+
+		let Type = await vscode.window.showQuickPick([ "Beautify", "Minify", "Uglify" ]);
+		if (!Type) return;
+
+		let Selected = await vscode.window.showQuickPick(Options, { canPickMany: true, placeHolder: "Pick options" });
+		if (!Selected) return;
+
+		let ParsedOptions = {};
+		Selected = Selected.forEach(o => ParsedOptions[o.label.split(" ").join("")] = true) // JUSt Use ReGex brO
+
+		let Output;
+		try {
+			Output = luamin[Type](Script, ParsedOptions);
+		} catch (er) {
+			console.log(er);
+			return vscode.window.showErrorMessage(`Failed to ${Type}`);
+		}
+
+		await vscode.workspace.openTextDocument({ content: Output, language: vscode.window.activeTextEditor.document.languageId }); // could be lua or luau
+		vscode.window.showInformationMessage("Completed! Output in new tab");
+	});
+
+	let Luamin = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
+	Luamin.text = "Luamin";
+	Luamin.command = "luamin.luamin";
+	Luamin.tooltip = "Lua Formatter";
+	Luamin.show();
+
+	context.subscriptions.push(command, Luamin);
+}
+
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
